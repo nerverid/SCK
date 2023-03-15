4fce153bc2a1ef9645b6cac93b9009e0e69011bf
nerverid <nero-add@mail.ru>
2023-03-15 18:00:48 +0700

    Edit UI quizeditor

--- src/UI/quizuieditor.py
+++ src/UI/quizuieditor.py
@@ -2,8 +2,19 @@ from tkinter import *
 
 class Application(Frame):
 
-    def __init__(self):
+    def __init__(self, master):
         super(Application,self).__init__(master)
+        self.grid()
+        self.create_widget()
 
     def create_widget(self):
-        pass
+        self.submit_bttn = Button(self, text="Submit")
+        self.submit_bttn.grid(row = 8, column = 3)
+
+root = Tk()
+root.title("Quiz editor")
+root.geometry("500x300")
+
+app = Application(root)
+
+root.mainloop() 
