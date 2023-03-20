03a83d25605d7f7db49489dd1679baa6dbb5c4e5
nerverid <nero-add@mail.ru>
2023-03-20 17:20:24 +0700

    Added 2 methods for change top windows

--- src/engine/searchbook.py
+++ src/engine/searchbook.py
@@ -1,1 +1,8 @@
 #Поиск литературы, составление списка из неё, предварительная сортировка самим модулем файлов по категориям.
+import os
+
+path = 'C:\\Users'
+
+print(os.listdir(path))
+for i in os.listdir(path):
+    print(i)
