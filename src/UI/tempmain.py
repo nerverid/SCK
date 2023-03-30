6e99d49a04198a1e214cce03d2919e97b7e0391b
nerverid <nero-add@mail.ru>
2023-03-30 15:00:05 +0700

    Edit main interface

---
+++ src/UI/tempmain.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+
+class Application:
+    def __init__(self, master):
+        self.master = master
+        self.master.geometry("400x300")
+        self.show_widgets()
+
+    def show_widgets(self):
+        self.frame = tk.Frame(self.master)
+        self.master.title("SCK")
+        
+        self.lbl = tk.Label(self.frame, text = "Test for today")
+        self.lbl.grid(row = 0, column = 0, columnspan = 2)
+        self.lbl.pack()
+
+        self.btn_lists = tk.Button(self.frame, text="Lists", command=self.open_list)
+        #self.btn_lists["command"] = self.open_list()
+        #self.btn_lists.grid(row = 1, column = 2, columnspan = 1)
+        self.btn_lists.pack()
+
+        self.frame.pack()
+        
+    def open_list(self):
+        global win_list
+        win_list = ListEditUI
+        win_list = tk.Toplevel(self.master)
+        win_list.focus()        
+        
+class QuizUI:
+    def show_widgets(self):
+        pass
+
+class ListEditUI(Application):
+    def show_widgets(self):
+        pass
+
+
+root = tk.Tk()
+app = Application(root)
+root.mainloop()
