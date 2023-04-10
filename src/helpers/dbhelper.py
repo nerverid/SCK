1be851f00a4b466487cbaf8038ce0331fa1dd6de
nerverid <nero-add@mail.ru>
2023-04-10 17:25:20 +0700

    Added database helper

--- src/helpers/dbhelper.py
+++ src/helpers/dbhelper.py
@@ -1,6 +1,18 @@
 #DB helper
 import mysql.connector
 
+mydb = mysql.connector.connect(host="localhost",
+                               user="root",
+                               password="DERgfhjkm1+")
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE mydatabase")
+
+for x in mycurcor:
+    print(x)
+print(mydb)
+
 def add_list():
     pass
 
